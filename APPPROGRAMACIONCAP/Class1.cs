3b0fc4340c67bb9ba0bf1e6b7f0d714bce734470
marcosangulo <angulomarcos97@gmail.com>
2019-11-14 09:45:33 -0500

    Agregar archivos de proyecto.

---
+++ APPPROGRAMACIONCAP/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APPPROGRAMACIONCAP
+{
+
+    /// <summary>
+    /// //Calcula el factorial de un numero 
+    /// </summary>
+    /// <param name="num"></param>
+    /// <returns></returns> 
+    public static class Class1
+    {
+        public  static long fact(int num)
+        {
+            long f = 1;
+            if (num == 0 || num == 1)
+                return f;
+
+            for (int i = 1; i <= num; i++)
+                f = f * i;
+            return f;
+
+        }
+
+    }
+}
